3dfdb21c7453cbc9572a01b70efc491fb4afa89f
kalen <jkalend@gmail.com>
2023-11-05 03:14:55 +0100

    added missing plant data

---
+++ itu/app/PlantData.ts
@@ -0,0 +1,49 @@
+export type PlantData = {
+    name: string,
+    nickname: string,
+    description: string,
+    image_path: string,
+    days: number,
+    watering_frequency: number,
+    watering_amount: number
+}
+export let Plant: PlantData[] = [{
+    "name": "aloe vera",
+    "nickname": "",
+    "description": "A good plant",
+    "image_path": "/aloe_vera.jpg",
+    "days": -2,
+    "watering_frequency": 5,
+    "watering_amount": 50
+},
+{
+    "name": "monstera adansonii",
+    "nickname": "Monstera",
+    "description": "A sad plant",
+    "image_path": "/monstera-adansonii.jpg",
+    "days": -1,
+    "watering_frequency": 3,
+    "watering_amount": 40
+},
+{
+    "name": "chlorophytum comosum",
+    "nickname": "Spider Plant",
+    "description": "A scary plant",
+    "image_path": "/spider_plant.jpg",
+    "days": 0,
+    "watering_frequency": 2,
+    "watering_amount": 30
+},
+{
+    "name": "draecena trifasciata",
+    "nickname": "Snake Plant",
+    "description": "Ssssss plant",
+    "image_path": "/snake_plant.png",
+    "days": 2,
+    "watering_frequency": 6,
+    "watering_amount": 60
+},]
+
+export async function getPlantDetail({name} : {name: string}) {
+    return Plant.find(plant => plant.name === name)
+}
